f84c1e11f6f95b6f02aeece34ca93236c5446272
Noemi Varela <noemivarelaq@gmail.com>
2014-11-17 16:56:18 +0100

    Clase para preguntas con varias respuestas

--- app/src/main/java/com/fistorias/android/pegadas/OptionListActivity.java
+++ app/src/main/java/com/fistorias/android/pegadas/OptionListActivity.java
@@ -1,7 +1,23 @@
 package com.fistorias.android.pegadas;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by noemi on 16/11/2014.
  */
-public class OptionListActivity {
+public class OptionListActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.option_list);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setTitle("Pregunta 1");
+        actionBar.setDisplayShowTitleEnabled(true);
+
+
+
+    }
 }
