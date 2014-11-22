a9e69923b87ad9b17d13fb0e5db90b64a1fefc10
Noemi Varela <noemivarelaq@gmail.com>
2014-11-22 18:02:26 +0100

    Clase para preguntas con varias respuestas

--- app/src/main/java/com/fistorias/android/pegadas/data/PegadasDBContract.java
+++ app/src/main/java/com/fistorias/android/pegadas/data/PegadasDBContract.java
@@ -16,22 +16,21 @@ public final class PegadasDBContract {
     private PegadasDBContract() {}
 
     public static abstract class Preguntas implements BaseColumns {
-        public static final String PREGUNTAS       = "preguntas";
-        public static final String ID = "id";
-        public static final String ID_CASO = "id_caso";
+        public static final String PREGUNTAS = "preguntas";
         public static final String ID_PREGUNTA = "id_pregunta";
+        public static final String NUM_CASO = "num_caso";
+        public static final String NUM_PREGUNTA = "num_pregunta";
         public static final String TIPO_PREGUNTA="tipo_pregunta";
         public static final String COD_IDIOMA = "cod_idioma";
         public static final String ENUNCIADO = "enunciado";
-
-
-
-        /*public static final String CREATE_TABLE = "CREATE TABLE " +
-                TABLE_NAME + " (" +
-                _ID + " INTEGER PRIMARY KEY," +
-                COLUMN_NAME_COL1 + TEXT_TYPE + COMMA_SEP +
-                COLUMN_NAME_COL2 + TEXT_TYPE + COMMA_SEP +
-                COLUMN_NAME_COL3 + TEXT_TYPE + COMMA_SEP + " )";
-        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;*/
     }
+    public static abstract class Respostas implements BaseColumns {
+        public static final String RESPOSTAS ="respostas";
+        public static final String ID_RESPOSTA = "id_resposta";
+        public static final String ID_PREGUNTA = "id_pregunta";
+        public static final String RESPOSTA="resposta";
+        public static final String E_RESPOSTA_CORRECTA = "e_resposta_correcta";
+        public static final String EXPLICACION = "explicacion";
+    }
+
 }
