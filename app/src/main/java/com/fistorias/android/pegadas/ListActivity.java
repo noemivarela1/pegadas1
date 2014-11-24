f4ff028985cb8d96b72b8a7642ea5d6a0ea71e8a
Noemi Varela <noemivarelaq@gmail.com>
2014-11-24 21:31:56 +0100

    Clase para preguntas con varias respuestas

--- app/src/main/java/com/fistorias/android/pegadas/ListActivity.java
+++ app/src/main/java/com/fistorias/android/pegadas/ListActivity.java
@@ -1,14 +1,15 @@
 package com.fistorias.android.pegadas;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
- * Created by noemi on 24/11/2014.
+ * Actividad para la lista de preguntas
  */
 public class ListActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_view_android_example);
+        setContentView(R.layout.questions);
     }
 }
